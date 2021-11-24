a576f8ea0ef6b8edebb10c78cc83e272c9d036b2
smithylee81 <smithylee81@yahoo.co.uk>
2021-11-24 12:49:56 +0000

    Add end and highscores HTML, CSS and JS files

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,7 +7,7 @@ const progressBarFull = document.querySelector('#progressBarFull');
 let currentQuestion = {}
 let acceptingAnswers = true;
 let score = 0;
-let questionsCounter = 0;
+let questionCounter = 0;
 let availableQuestions = []
 
 let questions = [
@@ -64,14 +64,14 @@ startGame = () =>{
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS){
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
     }
 
     questionsCounter++
-    progressText.innerText = `Question ${questionsCounter} of ${MAX_QUESTIONS}`
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionsCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
