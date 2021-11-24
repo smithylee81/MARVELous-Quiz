5fdfe383b03d280a384ac36a3a8ec4b4474e946f
smithylee81 <smithylee81@yahoo.co.uk>
2021-11-24 17:43:17 +0000

    Add styling and container div to quiz page

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,66 +1,66 @@
 const question = document.querySelector('#question');
-const question = Array.from(document.querySelector('.choice-text'));
+const choices = Array.from(document.querySelector('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
     {
-        question: 'What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?',
-        choice1: '2011',
-        choice2: '2008',
-        choice3: '1999',
-        choice4: '2005',
+        question: "What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?",
+        choice1: "2011",
+        choice2: "2008",
+        choice3: "1999",
+        choice4: "2005",
         answer: 2,
     },
     {
-        question: 'What is the name of Thor’s hammer?',
-        choice1: 'Blow Hammer',
-        choice2: 'Pixie',
-        choice3: 'Mjolnir',
-        choice4: 'Jane',
+        question: "What is the name of Thor’s hammer?",
+        choice1: "Blow Hammer",
+        choice2: "Pixie",
+        choice3: "Mjolnir",
+        choice4: "Jane",
         answer: 3,
     },
     {
-        question: 'What is Captain America’s shield made out of?',
-        choice1: 'Steel',
-        choice2: 'Kryptonite',
-        choice3: 'Platinum',
-        choice4: 'Vibranium',
+        question: "What is Captain America’s shield made out of?",
+        choice1: "Steel",
+        choice2: "Kryptonite",
+        choice3: "Platinum",
+        choice4: "Vibranium",
         answer: 4,
     },
     {
-        question: 'What country are Scarlet Witch and Quicksilver from?',
-        choice1: 'Sokovia.',
-        choice2: 'Bulgaria',
-        choice3: 'Russia',
-        choice4: 'Spain',
+        question: "What country are Scarlet Witch and Quicksilver from?",
+        choice1: "Sokovia",
+        choice2: "Bulgaria",
+        choice3: "Russia",
+        choice4: "Spain",
         answer: 1,
     },
     {
-        question: 'Before becoming Vision, what is the name of Iron Man’s A.I. butler?',
-        choice1: 'J.A.R.E.D.',
-        choice2: 'J.U.P.I.T.E.R.',
-        choice3: 'J.A.R.V.I.S.',
-        choice4: 'J.U.N.K.Y.A.R.D.',
+        question: "Before becoming Vision, what is the name of Iron Man’s A.I. butler?",
+        choice1: "J.A.R.E.D.",
+        choice2: "J.U.P.I.T.E.R.",
+        choice3: "J.A.R.V.I.S.",
+        choice4: "J.U.N.K.Y.A.R.D.",
         answer: 3,
-    },
+    }
 ]
 
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 5;
 
 startGame = () =>{
-    questionsCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
 getNewQuestion = () => {
@@ -70,28 +70,28 @@ getNewQuestion = () => {
         return window.location.assign('/end.html')
     }
 
-    questionsCounter++
+    questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionsCounter/MAX_QUESTIONS) * 100}%`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innetText = currentQuestion.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
-    } )
+    })
 
     availableQuestions.splice(questionsIndex, 1)
-    acceptingAnswers = true;
+    acceptingAnswers = true
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers) return
 
-        acceptingAnswers = false;
+        acceptingAnswers = false
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
@@ -106,15 +106,13 @@ choices.forEach(choice => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-
         }, 1000)
     })
-
 })
 
 incrementScore = num => {
-    score +=num;
-    scoreText.innerText = score;
+    score +=num
+    scoreText.innerText = score
 }
 
 startGame()
